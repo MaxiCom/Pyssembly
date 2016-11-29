294d30b96b691a42697f40ef31fec3b6a1823840
maxime morlet <maximemorlet@hotmail.fr>
2016-11-29 13:24:07 +0100

    Added error function

--- modules/core/converter.py
+++ modules/core/converter.py
@@ -2,6 +2,9 @@ from modules.global_instructions import *
 
 import struct
 
+def display_instruction_error(line_parsed):
+	display_error_message('[' + ' '.join(line_parsed) + ']' + ' Instruction unknown')
+
 def	convert_content(parsed_file_content):
 	buffer = ""
 	EAX_VALUE = 0
@@ -11,18 +14,18 @@ def	convert_content(parsed_file_content):
 		# MANAGE MOV INSTRUCTION
 		if line_parsed[0].lower() == 'mov':
 			if line_parsed[2].isdigit() == 0:
-				display_error_message('[' + ' '.join(line_parsed) + ']' + ' Instruction unknown')
+				display_instruction_error(line_parsed)
 
 			elif line_parsed[1].lower() == 'eax':
 				buffer += '66b8' + format(struct.unpack("<I", struct.pack(">I", int(line_parsed[2])))[0], 'x').zfill(8)
 				EAX_VALUE = int(line_parsed[2])
 			else:
-				display_error_message('[' + ' '.join(line_parsed) + ']' + ' Instruction unknown')
+				display_instruction_error(line_parsed)
 					
 		# MANAGE ADD INSTRUCTION
 		elif line_parsed[0].lower() == 'add':
 			if line_parsed[2].isdigit() == 0:
-				display_error_message('[' + ' '.join(line_parsed) + ']' + ' Instruction unknown')
+				display_instruction_error(line_parsed)
 
 			if line_parsed[1].lower() == 'eax':
 				if int(line_parsed[2]) <= 127:
@@ -32,11 +35,11 @@ def	convert_content(parsed_file_content):
 					buffer += '6605' + format(struct.unpack("<I", struct.pack(">I", int(line_parsed[2])))[0], 'x').zfill(8)
 					EAX_VALUE += int(line_parsed[2])
 			else:
-				display_error_message('[' + ' '.join(line_parsed) + ']' + ' Instruction unknown')
+				display_instruction_error(line_parsed)
 			
 
 		else:
-			display_error_message('[' + ' '.join(line_parsed) + ']' + ' Instruction unknown')
+				display_instruction_error(line_parsed)
 
 	display_success_message('Convert file content ... OK')	
 
