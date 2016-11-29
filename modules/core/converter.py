decbb06fea91a26dc27a518c40e3417ebcd08982
maxime morlet <maximemorlet@hotmail.fr>
2016-11-29 13:07:25 +0100

    Add instruction

--- modules/core/converter.py
+++ modules/core/converter.py
@@ -4,6 +4,7 @@ import struct
 
 def	convert_content(parsed_file_content):
 	buffer = ""
+	EAX_VALUE = 0
 
 	for line_parsed in parsed_file_content:
 	
@@ -12,16 +13,32 @@ def	convert_content(parsed_file_content):
 			if line_parsed[2].isdigit() == 0:
 				display_error_message('[' + ' '.join(line_parsed) + ']' + ' Instruction unknown')
 
-			if line_parsed[1].lower() == 'eax':
+			elif line_parsed[1].lower() == 'eax':
 				buffer += '66b8' + format(struct.unpack("<I", struct.pack(">I", int(line_parsed[2])))[0], 'x').zfill(8)
+				EAX_VALUE = int(line_parsed[2])
 			else:
 				display_error_message('[' + ' '.join(line_parsed) + ']' + ' Instruction unknown')
 					
+		# MANAGE ADD INSTRUCTION
+		elif line_parsed[0].lower() == 'add':
+			if line_parsed[2].isdigit() == 0:
+				display_error_message('[' + ' '.join(line_parsed) + ']' + ' Instruction unknown')
 
-
+			if line_parsed[1].lower() == 'eax':
+				if int(line_parsed[2]) <= 127:
+					buffer += '6683c0' + format(int(line_parsed[2]), 'x').zfill(2)
+					EAX_VALUE += int(line_parsed[2])
+				else:
+					buffer += '6605' + format(struct.unpack("<I", struct.pack(">I", int(line_parsed[2])))[0], 'x').zfill(8)
+					EAX_VALUE += int(line_parsed[2])
+			else:
+				display_error_message('[' + ' '.join(line_parsed) + ']' + ' Instruction unknown')
+			
 
 		else:
 			display_error_message('[' + ' '.join(line_parsed) + ']' + ' Instruction unknown')
 
 	display_success_message('Convert file content ... OK')	
+
+	print '\nFinal EAX value: %d\n' % EAX_VALUE
 	return buffer
--- modules/core/exporter.py
+++ modules/core/exporter.py
@@ -1,5 +1,8 @@
+from modules.global_instructions import *
+
 import binascii
 
 def	export(buffer):
 	with open('a.out', 'wb') as output_file:
 		output_file.write(binascii.unhexlify(buffer))
+	display_success_message('Export ... OK')
